b5fdecb1e85d96d5cf9505557aba05560cdc7ee9
ewhuet <ewhuet@gmail.com>
2023-10-24 14:56:13 +0300

    created add, subtract, multiply, divide functions

---
+++ app.js
@@ -0,0 +1,25 @@
+//Math functions to be used on calculator
+
+function add(num1, num2){
+    return num1 + num2;
+}
+
+function subtract(num1, num2){
+    return num1 - num2;
+}
+
+function multiply(num1, num2){
+    return num1 * num2;
+}
+
+function divide(num1, num2){
+    return num1 / num2;
+}
+
+//Variables to hold user inputs
+
+let num1 = 0
+let num2 = 0
+let operator = +;
+
+console.log(num1 + operator + num2);
